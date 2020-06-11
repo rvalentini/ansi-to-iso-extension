8a942880373d4e6a30bddb9314471c2990785ff8
rival <ri.valentini@reply.de>
2020-06-11 12:00:57 +0200

    Implement basic key mapping logic ANSI->ISO

--- ansi-to-iso.js
+++ ansi-to-iso.js
@@ -1,2 +1,36 @@
 const header = document.querySelector("div.page-header h1")
 header.textContent = header.textContent + " (for ISO keyboards)"
+
+
+const ansiToIso = {
+    ';': 'ö',
+    'i': 'Ö',
+    '\'': 'ä',
+    '"': 'Ä',
+    'y': 'z',
+    'Y': 'Z',
+    'z': 'y',
+    'Z': 'Y',
+    '<': ';',
+    '>': ':',
+    '/': '-',
+    '?': '_',
+    '[': 'ü',
+    ']': '+',
+    '{': 'Ü',
+    '}': '*',
+    '`': '^', //TODO trouble here with '^'
+    '~': '°',
+    '\\': '#',
+    '|': '\'',
+
+}
+
+const lessonChars = document.querySelectorAll('[id^="cc"]')
+
+Array.from(lessonChars).forEach(span => {
+    const char = span.innerHTML
+    if(char in ansiToIso) span.innerHTML = ansiToIso[span.innerHTML]
+})
+
+
